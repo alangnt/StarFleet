4c67f1de33eded6663b38220ef2c3af44bb7f9c4
alangnt <geirnaert.alan@icloud.com>
2025-10-04 21:25:36 +0200

    core: moved the backend into a separate folder

--- backend/Projects/urls.py
+++ backend/Projects/urls.py
@@ -18,11 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from frontend import homePage
+from Projects import views
 
 
 urlpatterns = [
-    path("", homePage.index, name="projects_index"),
+    path("", views.index, name="projects_index"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

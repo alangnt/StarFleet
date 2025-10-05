0e679b059b39d08a6031af4fefebdfc79082b2bc
alangnt <geirnaert.alan@icloud.com>
2025-10-05 22:17:00 +0200

    core: removed the backend folder and moved the nextjs project into a single project

--- backend/Projects/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from Projects.admins.ProjectAdmin import ProjectAdmin
-from Projects.models.ProjectModel import Project
-
-
-admin.site.register(Project, ProjectAdmin)
--- backend/Projects/admins/ProjectAdmin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-
-class ProjectAdmin(admin.ModelAdmin):
-    pass
